7e2695198c4bc0a959e5cb7756ad66e84609c512
daichi0410 <59831107+daichi0410@users.noreply.github.com>
2020-03-09 17:12:22 +0900

    jsの設定

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+  Particles.init({
+    selector: '.background',
+    sizeVariations: 30,
+    color: [
+      '#0bd', 'rgba(0,187,221,.5)', 'rgba(0,187,221,.2)'
+    ]
+  });
+  var particles = Particles.init({
+    selector: '.background',
+  sizeVariations: 15,
+  color: ['#00bbdd', '#404B69', '#DBEDF3'],
+  connectParticles: true
+});
+};
+
+
