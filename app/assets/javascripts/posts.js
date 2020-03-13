af01739c5c000070ff5591ed86fc3fd8f056a298
daichi0410 <59831107+daichi0410@users.noreply.github.com>
2020-03-13 12:29:03 +0900

    titleの編集

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,7 +1,7 @@
 window.onload = function() {
   Particles.init({
     selector: '.background',
-    sizeVariations: 30,
+    sizeVariations: 60,
     color: [
       '#0bd', 'rgba(0,187,221,.5)', 'rgba(0,187,221,.2)'
     ]
